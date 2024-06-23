062cd27a72a29d903dac06d039666e0f37108b58
samirm00 <sammoham.be@gmail.com>
2024-06-23 08:12:13 +0200

    Initial commit

---
+++ hello-world/data/phrases.js
@@ -0,0 +1,17 @@
+const phrases = [
+    {
+        id: 1,
+        en: 'Hello, World!',
+        es: '¡Hola, Mundo!',
+        fr: 'Bonjour, le Monde!',
+        de: 'Hallo, Welt!',
+        it: 'Ciao, Mondo!',
+        ja: 'こんにちは、世界！',
+        ko: '안녕하세요, 세계!',
+        pt: 'Olá, Mundo!',
+        ru: 'Привет, мир!',
+        zh: '你好，世界！'
+    }
+];
+
+export default phrases;
--- hello-world/main.js
+++ hello-world/main.js
@@ -1,5 +1,16 @@
-// DOM elements
-const header = document.querySelector('header');
-const title = document.createElement('h1');
-title.innerText = 'Hello World!';
-header.appendChild(title);
+import LangTranslate from 'lang-translate';
+import phrases from './data/phrases.js';
+
+// Dom elements
+const textDom = document.getElementById('phrase');
+const language = document.getElementById('language');
+
+const langTranslate = new LangTranslate('en', phrases);
+
+const text = langTranslate.translate(1);
+textDom.innerHTML = text;
+
+language.addEventListener('change', () => {
+    langTranslate.setLanguage(language.value);
+    textDom.innerHTML = langTranslate.translate(1);
+});
